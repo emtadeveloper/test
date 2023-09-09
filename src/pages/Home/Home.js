2f71c41b868bdc62bf716ba7d293821d2851dac3
emtadeveloper <emta137671@gmail.com>
2023-09-09 14:58:18 +0330

    commit 1 for 200

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,16 @@
-import React, { useContext } from 'react'
-import UserIndex from '../../components/User/UserIndex'
-import { UserContext } from '../../context'
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import UserIndex from "../../components/User/UserIndex";
+import { UserContext } from "../../context";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    const users = useContext(UserContext)
-    return (
-        <div>
-            {!users.loading && <Link to='/about-me'>
-                About
-            </Link>}
-            <UserIndex />
-        </div>
-    )
-}
+  const users = useContext(UserContext);
+  return (
+    <div>
+      {!users.loading && <Link to="/about-me">About emad</Link>}
+      <UserIndex />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
